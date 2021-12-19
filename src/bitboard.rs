be41488f52f74929aaedbfee56bd96ca7d4813c9
JonathanArns <joanthan.arns@googlemail.com>
2021-12-19 18:36:03 +0100

    fix bug where moves onto tails were falsly discarded

--- src/bitboard.rs
+++ src/bitboard.rs
@@ -234,32 +234,38 @@ impl<const N: usize> Bitboard<N> {
     }
 
     /// Can never return a move that moves out of bounds on the board,
-    /// that would cause a panic.
+    /// because that would cause a panic elsewhere.
     fn allowed_moves(&self, pos: u8) -> Vec<Move> {
         let mut moves = Vec::with_capacity(4);
         let mut some_legal_move = Move::Up;
+        let mut tails = [u8::MAX; N];
+        for i in 0..N {
+            if self.snakes[i].is_alive() && self.snakes[i].curled_bodyparts == 0 {
+                tails[i] = self.snakes[i].tail;
+            }
+        }
         let mask = 1<<pos;
         if pos > 10 {
             some_legal_move = Move::Down;
-            if self.bodies[0] & mask >> 11 == 0 {
+            if self.bodies[0] & mask >> 11 == 0 || tails.contains(&(pos - 11)) {
                 moves.push(Move::Down);
             }
         }
         if pos < 109 {
             some_legal_move = Move::Up;
-            if self.bodies[0] & mask << 11 == 0 {
+            if self.bodies[0] & mask << 11 == 0 || tails.contains(&(pos + 11)) {
                 moves.push(Move::Up);
             }
         }
         if pos % 11 > 0 {
             some_legal_move = Move::Left;
-            if self.bodies[0] & mask >> 1 == 0 {
+            if self.bodies[0] & mask >> 1 == 0 || tails.contains(&(pos - 1)) {
                 moves.push(Move::Left);
             }
         }
         if pos % 11 < 10 {
             some_legal_move = Move::Right;
-            if self.bodies[0] & mask << 1 == 0 {
+            if self.bodies[0] & mask << 1 == 0 || tails.contains(&(pos + 1)) {
                 moves.push(Move::Right);
             }
         }
@@ -429,5 +435,67 @@ impl<const N: usize> std::fmt::Debug for Bitboard<N> {
             f.write_str(&("head: ".to_string() + &snake.head.to_string() + " tail: " + &snake.tail.to_string() + " length: " + &snake.length.to_string() + " health: " + &snake.health.to_string() + "\n"))?;
         }
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::api;
+
+    fn c(x: usize, y: usize) -> api::Coord {
+        api::Coord{x, y}
+    }
+    
+    #[test]
+    fn test_weird_head_collision_deaths() {
+        let state = GameState{
+            game: api::Game{ id: "".to_string(), timeout: 100, ruleset: std::collections::HashMap::new() },
+            turn: 157,
+            you: api::Battlesnake{
+                id: "a".to_string(),
+                name: "a".to_string(),
+                latency: "".to_string(),
+                shout: None,
+                squad: None,
+                health: 100,
+                length: 11,
+                head: c(5,2),
+                body: vec![c(5,2), c(5,1), c(6, 1), c(7,1), c(7,2), c(8,2), c(8,3), c(7,3), c(7,4), c(6,4), c(6,4)],
+            },
+            board: api::Board{
+                height: 11,
+                width: 11,
+                food: vec![c(3,10), c(6,0), c(10,1), c(0,10), c(3,0), c(9,5), c(10,3), c(9,4), c(8,4), c(8,10), c(0,6)],
+                hazards: vec![],
+                snakes: vec![
+                    api::Battlesnake{
+                        id: "a".to_string(),
+                        name: "a".to_string(),
+                        latency: "".to_string(),
+                        shout: None,
+                        squad: None,
+                        health: 100,
+                        length: 11,
+                        head: c(5,2),
+                        body: vec![c(5,2), c(5,1), c(6, 1), c(7,1), c(7,2), c(8,2), c(8,3), c(7,3), c(7,4), c(6,4), c(6,4)],
+                    },  
+                    api::Battlesnake{
+                        id: "b".to_string(),
+                        name: "b".to_string(),
+                        latency: "".to_string(),
+                        shout: None,
+                        squad: None,
+                        health: 95,
+                        length: 12,
+                        head: c(3,4),
+                        body: vec![c(3,4), c(2,4), c(2,5), c(3, 5), c(3,6), c(3,7), c(3,8), c(4,8), c(4,7), c(4,6), c(4,5), c(4,4)],
+                    },  
+                ],
+            },
+        };
+        let mut game = Game{move_time: std::time::Duration::from_millis(state.game.timeout.into())};
+        let (mv, _) = Bitboard::<2>::from_gamestate(state).iterative_deepening_search(&mut game);
+        assert!(mv != Move::Up)
     }
 }
