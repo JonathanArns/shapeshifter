480e717f58655d44da410224c61240aaf42cbf26
JonathanArns <joanthan.arns@googlemail.com>
2022-02-11 17:37:36 +0100

    hide most board configs behind spl feature flag

--- src/api.rs
+++ src/api.rs
@@ -72,66 +72,6 @@ pub fn handle_start(_req: Json<GameState>) -> Status {
     Status::Ok
 }
 
-// #[cfg(feature = "mcts")]
-// #[post("/move", format = "json", data = "<req>")]
-// pub fn handle_move(req: Json<GameState>) -> JsonValue {
-//     let state = req.into_inner();
-//     let ruleset = match state.game.ruleset["name"].as_str() {
-//         Some("wrapped") => types::Ruleset::Wrapped,
-//         Some("royale") => types::Ruleset::Royale,
-//         Some("constrictor") => types::Ruleset::Constrictor,
-//         _ => types::Ruleset::Standard,
-//     };
-//     let mut game = types::Game{
-//         move_time: std::time::Duration::from_millis(state.game.timeout.into()),
-//         hazard_damage: 15,
-//         ruleset,
-//     };
-//     let (mv, _winrate) = match (state.board.snakes.len(), state.board.width, state.board.height) {
-//         (1, 7, 7) => mcts::search(&bitboard::Bitboard::<1, 7, 7>::from_gamestate(state, ruleset), &mut game),
-//         (2, 7, 7) => mcts::search(&bitboard::Bitboard::<2, 7, 7>::from_gamestate(state, ruleset), &mut game),
-//         (3, 7, 7) => mcts::search(&bitboard::Bitboard::<3, 7, 7>::from_gamestate(state, ruleset), &mut game),
-//         (4, 7, 7) => mcts::search(&bitboard::Bitboard::<4, 7, 7>::from_gamestate(state, ruleset), &mut game),
-
-//         (1, 11, 11) => mcts::search(&bitboard::Bitboard::<1, 11, 11>::from_gamestate(state, ruleset), &mut game),
-//         (2, 11, 11) => mcts::search(&bitboard::Bitboard::<2, 11, 11>::from_gamestate(state, ruleset), &mut game),
-//         (3, 11, 11) => mcts::search(&bitboard::Bitboard::<3, 11, 11>::from_gamestate(state, ruleset), &mut game),
-//         (4, 11, 11) => mcts::search(&bitboard::Bitboard::<4, 11, 11>::from_gamestate(state, ruleset), &mut game),
-//         (5, 11, 11) => mcts::search(&bitboard::Bitboard::<5, 11, 11>::from_gamestate(state, ruleset), &mut game),
-//         (6, 11, 11) => mcts::search(&bitboard::Bitboard::<6, 11, 11>::from_gamestate(state, ruleset), &mut game),
-//         (7, 11, 11) => mcts::search(&bitboard::Bitboard::<7, 11, 11>::from_gamestate(state, ruleset), &mut game),
-//         (8, 11, 11) => mcts::search(&bitboard::Bitboard::<8, 11, 11>::from_gamestate(state, ruleset), &mut game),
-
-//         (1, 19, 19) => mcts::search(&bitboard::Bitboard::<1, 19, 19>::from_gamestate(state, ruleset), &mut game),
-//         (2, 19, 19) => mcts::search(&bitboard::Bitboard::<2, 19, 19>::from_gamestate(state, ruleset), &mut game),
-//         (3, 19, 19) => mcts::search(&bitboard::Bitboard::<3, 19, 19>::from_gamestate(state, ruleset), &mut game),
-//         (4, 19, 19) => mcts::search(&bitboard::Bitboard::<4, 19, 19>::from_gamestate(state, ruleset), &mut game),
-//         (5, 19, 19) => mcts::search(&bitboard::Bitboard::<5, 19, 19>::from_gamestate(state, ruleset), &mut game),
-//         (6, 19, 19) => mcts::search(&bitboard::Bitboard::<6, 19, 19>::from_gamestate(state, ruleset), &mut game),
-//         (7, 19, 19) => mcts::search(&bitboard::Bitboard::<7, 19, 19>::from_gamestate(state, ruleset), &mut game),
-//         (8, 19, 19) => mcts::search(&bitboard::Bitboard::<8, 19, 19>::from_gamestate(state, ruleset), &mut game),
-
-//         (1, 25, 25) => mcts::search(&bitboard::Bitboard::<1, 25, 25>::from_gamestate(state, ruleset), &mut game),
-//         (2, 25, 25) => mcts::search(&bitboard::Bitboard::<2, 25, 25>::from_gamestate(state, ruleset), &mut game),
-//         (3, 25, 25) => mcts::search(&bitboard::Bitboard::<3, 25, 25>::from_gamestate(state, ruleset), &mut game),
-//         (4, 25, 25) => mcts::search(&bitboard::Bitboard::<4, 25, 25>::from_gamestate(state, ruleset), &mut game),
-//         (5, 25, 25) => mcts::search(&bitboard::Bitboard::<5, 25, 25>::from_gamestate(state, ruleset), &mut game),
-//         (6, 25, 25) => mcts::search(&bitboard::Bitboard::<6, 25, 25>::from_gamestate(state, ruleset), &mut game),
-//         (7, 25, 25) => mcts::search(&bitboard::Bitboard::<7, 25, 25>::from_gamestate(state, ruleset), &mut game),
-//         (8, 25, 25) => mcts::search(&bitboard::Bitboard::<8, 25, 25>::from_gamestate(state, ruleset), &mut game),
-//         (9, 25, 25) => mcts::search(&bitboard::Bitboard::<9, 25, 25>::from_gamestate(state, ruleset), &mut game),
-//         (10, 25, 25) => mcts::search(&bitboard::Bitboard::<10, 25, 25>::from_gamestate(state, ruleset), &mut game),
-//         (11, 25, 25) => mcts::search(&bitboard::Bitboard::<11, 25, 25>::from_gamestate(state, ruleset), &mut game),
-//         (12, 25, 25) => mcts::search(&bitboard::Bitboard::<12, 25, 25>::from_gamestate(state, ruleset), &mut game),
-//         (13, 25, 25) => mcts::search(&bitboard::Bitboard::<13, 25, 25>::from_gamestate(state, ruleset), &mut game),
-//         (14, 25, 25) => mcts::search(&bitboard::Bitboard::<14, 25, 25>::from_gamestate(state, ruleset), &mut game),
-//         (15, 25, 25) => mcts::search(&bitboard::Bitboard::<15, 25, 25>::from_gamestate(state, ruleset), &mut game),
-//         (16, 25, 25) => mcts::search(&bitboard::Bitboard::<16, 25, 25>::from_gamestate(state, ruleset), &mut game),
-//         _ => panic!("Snake count or board size not supported S: {}, W: {}, H: {}", state.board.snakes.len(), state.board.width, state.board.height),
-//     };
-//     mv.to_json()
-// }
-
 #[post("/move", format = "json", data = "<req>")]
 pub fn handle_move(req: Json<GameState>) -> JsonValue {
     let state = req.into_inner();
@@ -142,6 +82,22 @@ pub fn handle_move(req: Json<GameState>) -> JsonValue {
         Some("constrictor") => types::Ruleset::Constrictor,
         _ => types::Ruleset::Standard,
     };
+
+    #[cfg(not(feature = "spl"))]
+    let (mv, _score, _depth) = match (state.board.snakes.len(), state.board.width, state.board.height, matches!(ruleset, types::Ruleset::Wrapped)) {
+        (1, 11, 11, true) => minimax::search(&bitboard::Bitboard::<1, 11, 11, true>::from_gamestate(state), deadline),
+        (2, 11, 11, true) => minimax::search(&bitboard::Bitboard::<2, 11, 11, true>::from_gamestate(state), deadline),
+        (3, 11, 11, true) => minimax::search(&bitboard::Bitboard::<3, 11, 11, true>::from_gamestate(state), deadline),
+        (4, 11, 11, true) => minimax::search(&bitboard::Bitboard::<4, 11, 11, true>::from_gamestate(state), deadline),
+
+        (1, 11, 11, false) => minimax::search(&bitboard::Bitboard::<1, 11, 11, false>::from_gamestate(state), deadline),
+        (2, 11, 11, false) => minimax::search(&bitboard::Bitboard::<2, 11, 11, false>::from_gamestate(state), deadline),
+        (3, 11, 11, false) => minimax::search(&bitboard::Bitboard::<3, 11, 11, false>::from_gamestate(state), deadline),
+        (4, 11, 11, false) => minimax::search(&bitboard::Bitboard::<4, 11, 11, false>::from_gamestate(state), deadline),
+        _ => panic!("Snake count or board size not supported S: {}, W: {}, H: {}, please enable the 'spl' feature.", state.board.snakes.len(), state.board.width, state.board.height),
+    };
+
+    #[cfg(feature = "spl")]
     let (mv, _score, _depth) = match (state.board.snakes.len(), state.board.width, state.board.height, matches!(ruleset, types::Ruleset::Wrapped)) {
         (1, 7, 7, true) => minimax::search(&bitboard::Bitboard::<1, 7, 7, true>::from_gamestate(state), deadline),
         (2, 7, 7, true) => minimax::search(&bitboard::Bitboard::<2, 7, 7, true>::from_gamestate(state), deadline),
