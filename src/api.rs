bbd1058ecd9e68feb5b35d18edbb14fd7d48b69e
JonathanArns <jonathan.arns@googlemail.com>
2022-03-11 19:05:17 +0100

    decrease latency tolerance to 80ms

--- src/api.rs
+++ src/api.rs
@@ -83,7 +83,7 @@ pub fn handle_end() -> Status {
 #[post("/move", format = "json", data = "<req>")]
 pub fn handle_move(req: Json<GameState>) -> JsonValue {
     let state = req.into_inner();
-    let deadline = time::Instant::now() + time::Duration::from_millis(((state.game.timeout / 2).max(state.game.timeout.max(150) - 150)).into());
+    let deadline = time::Instant::now() + time::Duration::from_millis(((state.game.timeout / 2).max(state.game.timeout.max(80) - 80)).into());
     let ruleset = match state.game.ruleset["name"].as_str() {
         Some("wrapped") => types::Ruleset::Wrapped,
         Some("royale") => types::Ruleset::Royale,
@@ -219,7 +219,7 @@ pub fn handle_move(req: Json<GameState>) -> JsonValue {
 #[post("/move", format = "json", data = "<req>")]
 pub fn handle_move(req: Json<GameState>) -> JsonValue {
     let state = req.into_inner();
-    let deadline = time::Instant::now() + time::Duration::from_millis(((state.game.timeout / 2).max(state.game.timeout.max(150) - 150)).into());
+    let deadline = time::Instant::now() + time::Duration::from_millis(((state.game.timeout / 2).max(state.game.timeout.max(80) - 80)).into());
     let ruleset = match state.game.ruleset["name"].as_str() {
         Some("wrapped") => types::Ruleset::Wrapped,
         Some("royale") => types::Ruleset::Royale,
