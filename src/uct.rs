40428f4e57a8155eab88c633b0329c931574d500
JonathanArns <jonathan.arns@googlemail.com>
2022-05-29 16:56:31 +0200

    refactor mcts implementation to preallocate all memory

--- src/uct.rs
+++ src/uct.rs
@@ -5,8 +5,6 @@ use arrayvec::ArrayVec;
 use rand::Rng;
 use rand_pcg::Pcg64Mcg;
 use std::time;
-use std::rc::{Rc, Weak};
-use std::cell::RefCell;
 
 enum Moves<const S: usize> {
     Me(ArrayVec<Move, 4>),
@@ -32,10 +30,11 @@ struct Node<const S: usize, const W: usize, const H: usize, const WRAP: bool>
 where [(); (W*H+63)/64]: Sized {
     max: bool,
     board: Bitboard<S, W, H, WRAP>,
-    parent: Option<Weak<RefCell<Node<S, W, H, WRAP>>>>,
+    idx: usize,
+    parent: Option<usize>,
     moves_idx: usize,
     moves: Moves<S>,
-    children: Vec<Option<Rc<RefCell<Node<S, W, H, WRAP>>>>>,
+    children: Vec<Option<usize>>,
     visits: u32,
     wins: u32,
     value: f64,
@@ -43,7 +42,7 @@ where [(); (W*H+63)/64]: Sized {
 
 impl<const S: usize, const W: usize, const H: usize, const WRAP: bool> Node<S, W, H, WRAP> 
 where [(); (W*H+63)/64]: Sized {
-    fn new(board: Bitboard<S, W, H, WRAP>, moves_idx: usize, parent: Option<Weak<RefCell<Self>>>, max: bool) -> Self {
+    fn new(board: Bitboard<S, W, H, WRAP>, idx: usize, moves_idx: usize, parent: Option<usize>, max: bool) -> Self {
         // this is effectively the move generation for the in memory tree
         let moves = if max {
             Moves::Me(allowed_moves(&board, board.snakes[0].head))
@@ -54,6 +53,7 @@ where [(); (W*H+63)/64]: Sized {
         Node::<S, W, H, WRAP>{
             max,
             board,
+            idx,
             parent,
             moves_idx,
             children: vec![None; moves.len()],
@@ -65,20 +65,20 @@ where [(); (W*H+63)/64]: Sized {
     }
 }
 
-fn expand<const S: usize, const W: usize, const H: usize, const WRAP: bool>(node: Rc<RefCell<Node<S, W, H, WRAP>>>, moves_idx: usize) -> Rc<RefCell<Node<S, W, H, WRAP>>>
+fn expand<const S: usize, const W: usize, const H: usize, const WRAP: bool>(tree: &mut Vec<Node<S, W, H, WRAP>>, node_idx: usize, moves_idx: usize) -> usize
 where [(); (W*H+63)/64]: Sized {
-    let mut board = node.borrow().board.clone();
-    if !node.borrow().max {
+    let mut board = tree[node_idx].board.clone();
+    if !tree[node_idx].max {
         // get enemy moves from node
-        let mut moves = if let Moves::Enemies(mvs) = &node.borrow().moves {
+        let mut moves = if let Moves::Enemies(mvs) = &tree[node_idx].moves {
             mvs[moves_idx]
         } else {
             panic!("Min node does not have enemies moves");
         };
         // get my move from parent
-        moves[0] = if let Some(parent) = &node.borrow().parent {
-            if let Moves::Me(mvs) = &parent.upgrade().unwrap().borrow().moves {
-                mvs[node.borrow().moves_idx]
+        moves[0] = if let Some(parent_idx) = tree[node_idx].parent {
+            if let Moves::Me(mvs) = &tree[parent_idx].moves {
+                mvs[tree[node_idx].moves_idx]
             } else {
                 panic!("Min node's parent does not have me moves")
             }
@@ -87,86 +87,87 @@ where [(); (W*H+63)/64]: Sized {
         };
         board.apply_moves(&moves);
     }
-    let new = Rc::new(RefCell::new(Node::<S, W, H, WRAP>::new(board, moves_idx, Some(Rc::downgrade(&node)), !node.borrow().max)));
-    node.borrow_mut().children[moves_idx] =  Some(Rc::clone(&new));
-    new
+    let idx = tree.len();
+    let new = Node::<S, W, H, WRAP>::new(board, idx, moves_idx, Some(tree[node_idx].idx), !tree[node_idx].max);
+    tree.push(new);
+    tree[node_idx].children[moves_idx] = Some(idx);
+    idx
 }
 
 pub fn search<const S: usize, const W: usize, const H: usize, const WRAP: bool>(board: &Bitboard<S, W, H, WRAP>, deadline: time::Instant) -> (Move, f64)
 where [(); (W*H+63)/64]: Sized {
-    let mut node_counter = 0;
+    let mut tree = Vec::<Node<S, W, H, WRAP>>::with_capacity(1000);
     let mut rng = Pcg64Mcg::new(91825765198273048172569872943871926276_u128);
+    let mut node_counter = 0;
     let start_time = time::Instant::now();
-    let root = Rc::new(RefCell::new(Node::<S, W, H, WRAP>::new(board.clone(), 0, None, true)));
+
+    let root = Node::<S, W, H, WRAP>::new(board.clone(), 0, 0, None, true);
+    tree.push(root);
     while time::Instant::now() < deadline {
-        once(Rc::clone(&root), &mut rng, &mut node_counter);
+        once(&mut tree, &mut rng, &mut node_counter);
     }
-    let moves = if let Moves::Me(mvs) = &root.borrow().moves {
+    let moves = if let Moves::Me(mvs) = &tree[0].moves {
         mvs.clone()
     } else {
         panic!("search root does not have me moves");
     };
     let mut best_winrate = 0_f64;
     let mut best_move = Move::Up;
-    for (i, child) in root.borrow().children.iter().enumerate() {
-        if let Some(node) = child {
-            let winrate = node.borrow().wins as f64 / node.borrow().visits as f64;
+    for (i, child) in tree[0].children.iter().enumerate() {
+        if let Some(node_idx) = child {
+            let winrate = tree[*node_idx].wins as f64 / tree[*node_idx].visits as f64;
             if winrate > best_winrate {
                 best_winrate = winrate;
                 best_move = moves[i];
             }
-            print!("({:?}:{}:{})", root.borrow().moves.get_my_move(node.borrow().moves_idx), node.borrow().wins, node.borrow().visits);
+            print!("({:?}:{}:{})", tree[0].moves.get_my_move(tree[*node_idx].moves_idx), tree[*node_idx].wins, tree[*node_idx].visits);
         }
     }
     println!("\n{:?} nodes total, {:?} nodes per second", node_counter, node_counter as u128 * (time::Duration::from_secs(1).as_nanos() / start_time.elapsed().as_nanos()));
     (best_move, best_winrate)
 }
 
-fn once<const S: usize, const W: usize, const H: usize, const WRAP: bool>(root: Rc<RefCell<Node<S, W, H, WRAP>>>, rng: &mut impl Rng, node_counter: &mut u64)
+fn once<const S: usize, const W: usize, const H: usize, const WRAP: bool>(tree: &mut Vec<Node<S, W, H, WRAP>>, rng: &mut impl Rng, node_counter: &mut u64)
 where [(); (W*H+63)/64]: Sized {
     // select
-    let mut node = root;
-    let mut moves_idx = select_child(Rc::clone(&node));
+    let mut node_idx = 0;
+    let mut moves_idx = select_child(tree, node_idx);
     loop {
-        let child;
-        if let Some(tmp) = &node.borrow().children[moves_idx] {
-            child = Rc::clone(&tmp);
+        if let Some(idx) = tree[node_idx].children[moves_idx] {
+            node_idx = idx;
         } else {
             break
         };
-        node = child;
-        moves_idx = select_child(Rc::clone(&node));
+        moves_idx = select_child(tree, node_idx);
     }
 
-    node = expand(node, moves_idx);
-    moves_idx = select_child(Rc::clone(&node));
+    node_idx = expand(tree, node_idx, moves_idx);
+    moves_idx = select_child(tree, node_idx);
 
     // simulate
-    let result = playout(Rc::clone(&node), moves_idx, rng, node_counter);
+    let result = playout(tree, node_idx, moves_idx, rng, node_counter);
     
     // propagate
     loop {
         // TODO: deal with terminal nodes in the tree somehow somewhere
         // TODO: treat min nodes correctly?
-        node.borrow_mut().visits += 1;
-        if (result == 1 && !node.borrow().max) || (result == -1 && node.borrow().max) {
-            node.borrow_mut().wins += 1;
+        tree[node_idx].visits += 1;
+        if (result == 1 && !tree[node_idx].max) || (result == -1 && tree[node_idx].max) {
+            tree[node_idx].wins += 1;
         }
-        let parent;
-        if let Some(tmp) = &node.borrow().parent {
-            parent = Weak::clone(tmp);
+        if let Some(idx) = tree[node_idx].parent {
+            node_idx = idx;
         } else {
             break
         };
-        node = parent.upgrade().unwrap();
     }
 }
 
 // returns the winner's snake index
-fn playout<const S: usize, const W: usize, const H:usize, const WRAP: bool>(node: Rc<RefCell<Node<S, W, H, WRAP>>>, moves_idx: usize, rng: &mut impl Rng, node_counter: &mut u64) -> i8
+fn playout<const S: usize, const W: usize, const H:usize, const WRAP: bool>(tree: &mut Vec<Node<S, W, H, WRAP>>, node_idx: usize, moves_idx: usize, rng: &mut impl Rng, node_counter: &mut u64) -> i8
 where [(); (W*H+63)/64]: Sized {
-    let mut board = node.borrow().board.clone();
-    let mut moves = match &node.borrow().moves {
+    let mut board = tree[node_idx].board.clone();
+    let mut moves = match &tree[node_idx].moves {
         Moves::Me(mvs) => {
             let mut tmp = random_move_combination(&board, rng);
             tmp[0] = mvs[moves_idx];
@@ -174,8 +175,8 @@ where [(); (W*H+63)/64]: Sized {
         },
         Moves::Enemies(mvs) => {
             let mut tmp = mvs[moves_idx];
-            tmp[0] = if let Moves::Me(ref mvs) = node.borrow().parent.as_ref().unwrap().upgrade().unwrap().borrow().moves {
-                mvs[node.borrow().moves_idx]
+            tmp[0] = if let Moves::Me(ref mvs) = tree[tree[node_idx].parent.unwrap()].moves {
+                mvs[tree[node_idx].moves_idx]
             } else {
                 panic!("parent node of min node does not have me moves");
             };
@@ -198,14 +199,15 @@ where [(); (W*H+63)/64]: Sized {
     return 0
 }
 
-fn select_child<const S: usize, const W: usize, const H: usize, const WRAP: bool>(node: Rc<RefCell<Node<S, W, H, WRAP>>>) -> usize
+fn select_child<const S: usize, const W: usize, const H: usize, const WRAP: bool>(tree: &Vec<Node<S, W, H, WRAP>>, node: usize) -> usize
 where [(); (W*H+63)/64]: Sized {
-    let parent_visits = node.borrow().visits;
+    let parent_visits = tree[node].visits;
     let mut best_val = 0_f64;
     let mut best_moves_idx = 0;
-    for (i, x) in node.borrow().children.iter().enumerate() {
-        if let Some(child) = x {
-            let val = ucb1(parent_visits.into(), child.borrow().visits.into(), child.borrow().wins.into());
+    for (i, x) in tree[node].children.iter().enumerate() {
+        if let Some(child_idx) = x {
+            let child = &tree[*child_idx];
+            let val = ucb1(parent_visits.into(), child.visits.into(), child.wins.into());
             if best_val < val {
                 best_val = val;
                 best_moves_idx = i;
